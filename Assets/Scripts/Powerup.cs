5c471e031784282f2bd49af1fd7ec16a16e80283
Christopher Madrigal <chrisjmadrigal@gmail.com>
2022-05-13 20:35:38 -0700

    add sounds and a dynamic skybox material

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class Powerup : MonoBehaviour
 {
-    // customize me!
+    public AudioClip powerupClip;
+
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Player")
         {
             // do something to power them up!
             collider.gameObject.GetComponent<PlayerController>().bombExplosionRange++;
+            AudioSource.PlayClipAtPoint(powerupClip, transform.position);
             Destroy(gameObject, 0f);
         }
     }
